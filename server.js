72df1e24294b8f486be60e4ac25a2d2d48d0197d
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-06-28 04:19:33 +0600

    installing nodemon

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
  const express = require('express');
  const app = express();
 
- app.get('/', function(req, res) {
-     res.send('Hello World');
+ //Routes
+ app.get('/', home);
+ app.get('/home', helloWorld);
+ app.get('/about', about);
 
- })
+
+
+ //Controllers
+ function home(req, res) {
+     res.send('Welcome Home');
+ }
+
+ function helloWorld(req, res) {
+     res.send('Hello World');
+ }
+
+ function about(req, res) {
+     res.send('About me');
+ }
+
+ //sets the port and runs the app
  app.listen(3000);
