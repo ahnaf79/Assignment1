540779b90d14edf52b306cb8d23bb6c6469e3674
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-06-30 09:12:07 +0600

    setting header

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
- const express = require('express');
- const app = express();
-
- //Routes
- app.get('/', home);
- app.get('/about', about);
- app.get('/projects', projects);
- app.get('/contact', contact);
+#!/usr/bin/env node
 
 
- //Controllers
- function home(req, res) {
-     res.send('Home');
- }
 
- function about(req, res) {
-     res.send('About me');
- }
+const express = require('express');
+const app = express();
 
- function projects(req, res) {
-     res.send('Projects page');
- }
-
- function contact(req, res) {
-     res.send('Contact Me');
- }
+//Routes
+app.get('/', home);
+app.get('/about', about);
+app.get('/projects', projects);
+app.get('/contact', contact);
 
 
- //sets the port and runs the app
- app.listen(3000);
+//Controllers
+function home(req, res) {
+    res.send('Home');
+}
+
+function about(req, res) {
+    res.send('About me');
+}
+
+function projects(req, res) {
+    res.send('Projects page');
+}
+
+function contact(req, res) {
+    res.send('Contact Me');
+}
+
+
+//sets the port and runs the app
+app.listen(3000);
