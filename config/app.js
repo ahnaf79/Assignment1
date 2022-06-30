a1323f311354679fabe9385fdb37d6a72b0dafdf
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-06-30 12:05:46 +0600

    updating pages: contact.ejs, home.ejs and projects.ejs

--- config/app.js
+++ config/app.js
@@ -15,13 +15,13 @@ var app = express();
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../../node_modules")));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
