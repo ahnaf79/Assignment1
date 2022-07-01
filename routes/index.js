1573eda0e5a067091f5b60b3e67ee08ff2695ce5
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-07-01 23:25:50 +0600

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+let indexRouteController = require("../controllers/index");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", indexRouteController.routeToHomePage);
+/* GET home page. */
+router.get("/home", indexRouteController.routeToHomePage);
+/* GET aboutme page. */
+router.get("/aboutme", indexRouteController.routeToAboutMe);
+/* GET projects page. */
+router.get("/projects", indexRouteController.routeToProjects);
+/* GET services page. */
+router.get("/services", indexRouteController.routeToServices);
+/* GET contact page. */
+router.get("/contactme", indexRouteController.routeToContactMe);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+router.get('/', userspage);
 
-router.get('/show', showUser);
 
-function showUser(req, res, next) {
+function userspage(req, res, next) {
     res.render(
         'users', {
-            title: 'Express',
+            title: 'Users',
             userName: 'Ahnaf'
         }
 
